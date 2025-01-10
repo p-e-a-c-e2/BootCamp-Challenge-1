fcfa13f425079cca02c933359a54ded8ada81910
p-e-a-c-e2 <smartpeace6@gmail.com>
2025-01-10 16:20:57 +0100

    update

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 
-// const milestoneDate = new Date("January 14, 2025 10:00:00").getTime();
 
-// function updateCountdown() {
-//   const now = new Date().getTime();
-//   const distance = milestoneDate - now;
-
-//   if (distance < 0) {
-//     document.getElementById("timer").innerHTML = "<p>The milestone is here!</p>";
-//     clearInterval(countdownInterval);
-//     return;
-//   }
-
-//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   // Pad numbers to 2 digits
-//   document.getElementById("days").innerText = String(days).padStart(2, "0");
-//   document.getElementById("hours").innerText = String(hours).padStart(2, "0");
-//   document.getElementById("minutes").innerText = String(minutes).padStart(2, "0");
-//   document.getElementById("seconds").innerText = String(seconds).padStart(2, "0");
-// }
-
-// const countdownInterval = setInterval(updateCountdown, 1000);
-// updateCountdown();
 const milestoneDate = new Date("January 14, 2025 10:00:00").getTime();
 
 let countdownInterval;
